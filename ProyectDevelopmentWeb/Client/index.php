eb0db8566278699f5336dc4c30b939c37f157b22
David Díaz <diazdavid.info@gmail.com>
2015-01-21 07:38:25 +0100

    Signed-off-by: David Díaz <diazdavid.info@gmail.com>

--- ProyectDevelopmentWeb/Client/index.php
+++ ProyectDevelopmentWeb/Client/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<script type="text/javascript" src="js/Classes/League.js"></script>
+	<script type="text/javascript" src="js/Classes/Classes.js"></script>
 	<script type="text/javascript" src="js/Utils/Utils.js"></script>
 	
 	<script type="text/javascript" src="js/Function/Function.js"></script>
--- ProyectDevelopmentWeb/Client/js/Utils/Utils.js
+++ ProyectDevelopmentWeb/Client/js/Utils/Utils.js
@@ -1,24 +1,15 @@
-/**
- * 
- */
 
+// Extensión del objeto window para añadir constantes globales.
 window.Constant = {
 	URL_SERVER: function() { return "../Server/Services/Api.php"; },
 	XMLHTTP_CLIENT: function() { return (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"); }
 }
 
-//function Utils () {
-//	parseJsonToObject = function(data, obj) {
-//		console.log(data);
-//		console.log(obj);
-//	}
-//}
-
+// Extensión del objeto window para añadir funciones globales.
 window.Utils = {
 		ParseJsonToObjct: function(data, obj) {
 			var json = JSON.parse(data);
-			console.log(json.id);
-			return new obj(data.id, data.nombre, data.ayno_inicio, data.ayno_fin);
+			return new obj(json[0].id, data.nombre, data.ayno_inicio, data.ayno_fin);
 			
 		}
 }
